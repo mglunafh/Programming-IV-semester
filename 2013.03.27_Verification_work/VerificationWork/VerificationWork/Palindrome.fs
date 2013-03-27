522e1127f0b7b8b146420e54880a98b65473565e
mglunafh <ufispb@gmail.com>
2013-03-27 12:34:02 +0300

    here is a function searching the most palindrome number of special type.

---
+++ 2013.03.27_Verification_work/VerificationWork/VerificationWork/Palindrome.fs
@@ -0,0 +1,30 @@
+﻿module Palindrome
+// to find a palindrome number which can be obtained by product of a couple of three-digit numbers.
+
+let isPalindrome n =
+    let rec digits number i acc = 
+        if number > 0 then 
+            digits (number / 10) (i + 1) ((number % 10) :: acc) 
+        else 
+            (acc, List.rev acc)
+    
+    let rec check lists = 
+        match lists with
+        | (hd1 :: tl1, hd2 :: tl2) -> if hd1 = hd2 then check (tl1, tl2) else false
+        | ([], []) -> true
+        | (_, _) -> failwith "Потеряли элементы, пока реверсили список, .NET отстой"
+
+    check (digits n 0 [])
+
+let lookingForMost index1 index2 =
+    let rec lookingFor ind1 ind2 acc = 
+        let nextAcc = 
+            let t = ind1 * ind2 
+            if isPalindrome t && t > acc then t else acc
+        if ind1 < 999 then 
+            lookingFor (ind1 + 1) ind2 nextAcc
+        elif ind2 < 999 then 
+            lookingFor (ind2 + 1) (ind2 + 1) nextAcc
+        else 
+            acc
+    f index1 index2 0

6f55ae5af203b989f5e8235794159acaef518854
mglunafh <ufispb@gmail.com>
2013-03-27 12:51:27 +0300

    super-puper-map

---
+++ 2013.03.27_Verification_work/VerificationWork/VerificationWork/Supermap.fs
@@ -0,0 +1,10 @@
+﻿module Supermap
+
+let flatten list =
+    let rec flatten_aux l acc = 
+        match l with 
+        | l1 :: ls -> flatten_aux ls (acc @ l1)
+        | [] -> acc
+    flatten_aux list []
+
+let superMap list f = flatten (List.map f list)

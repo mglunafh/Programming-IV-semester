f06a48c8658bdb8ac034f346b9d4cb0dc8d1fcfc
mglunafh <ufispb@gmail.com>
2013-04-10 11:53:07 +0400

    stack in imperative style

---
+++ 2013.04.10_Verification_work/VerificationWork/VerificationWork/Stack.fs
@@ -0,0 +1,16 @@
+﻿
+type Stack (initStack) = 
+    let mutable mutStack = initStack
+    
+    member s.Push value =
+        mutStack <- value :: mutStack
+    
+    member s.Pop = 
+        match mutStack with 
+        |top :: rest -> 
+            mutStack <- rest
+            top
+        |[] -> failwith "attempt to call method 'Stack.Pop' from empty stack."
+    
+    member s.IsEmpty = (List.length mutStack = 0)
+    new() = Stack []

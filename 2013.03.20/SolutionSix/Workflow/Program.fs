069a20be3d54c29fac9b9dfea6f7bd3e88e055ad
mglunafh <ufispb@gmail.com>
2013-04-28 19:49:51 +0400

    function 'rounding' was added

--- 2013.03.20/SolutionSix/Workflow/Program.fs
+++ 2013.03.20/SolutionSix/Workflow/Program.fs
@@ -1,3 +1,5 @@
 ﻿type RoundingBuilder(n : int) = 
     member this.Bind(x : float, rest : float -> float) = rest x
     member this.Return(x : float) = System.Math.Round(x, n)
+
+let rounding = fun n -> RoundingBuilder n

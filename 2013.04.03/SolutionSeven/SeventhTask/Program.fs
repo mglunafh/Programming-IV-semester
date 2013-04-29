54526c4370c42a72df3089b6a05e0d9cdec43a92
mglunafh <ufispb@gmail.com>
2013-04-29 04:48:49 +0400

    Function calculating a sum of elements in array via many-threading. HUUUUUGE LOSSES 544 vs. 1 000 000

---
+++ 2013.04.03/SolutionSeven/SeventhTask/Program.fs
@@ -0,0 +1,18 @@
+﻿open System.ComponentModel
+
+let mutable threadResult = 0
+let mutable dumbResult = 0 
+
+let countOnes multiplyer n = 
+    let buffer = Array.init (n * multiplyer) (fun i -> 1)
+    
+    Array.iter (fun i -> dumbResult <- dumbResult + 1) buffer
+
+    for i in 0..n do 
+        let worker = new BackgroundWorker()
+        do worker.DoWork.Add(fun args -> 
+            let rec addPart j = if j < multiplyer then threadResult <- threadResult + buffer.[1000 * i + j]
+            addPart 0)
+        worker.RunWorkerAsync()
+
+    printfn "%d vs %d" threadResult dumbResult

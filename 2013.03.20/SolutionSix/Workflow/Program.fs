5de7cf6edc99a3e2a3a88fe5364a23339ea26f46
Fedor Uvarychev <ufispb@gmail.com>
2013-09-15 20:25:48 +0400

    'rounding now should work correctly

--- 2013.03.20/SolutionSix/Workflow/Program.fs
+++ 2013.03.20/SolutionSix/Workflow/Program.fs
@@ -1,5 +1,5 @@
 ﻿type RoundingBuilder(n : int) = 
-    member this.Bind(x : float, rest : float -> float) = rest x
+    member this.Bind(x : float, rest : float -> float) = rest (System.Math.Round(x, n))
     member this.Return(x : float) = System.Math.Round(x, n)
 
 let rounding = fun n -> RoundingBuilder n

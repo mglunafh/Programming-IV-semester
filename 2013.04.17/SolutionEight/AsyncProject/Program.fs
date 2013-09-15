e3b73e84e92c149209a8f0c8dc22300ee6faef84
Fedor Uvarychev <ufispb@gmail.com>
2013-09-15 20:14:05 +0400

    asynchronous downloading of links

---
+++ 2013.04.17/SolutionEight/AsyncProject/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+open System.IO
+open System.Net
+open Microsoft.FSharp.Control.WebExtensions
+open System.Text.RegularExpressions
+
+let asyncFetch (url : string) = 
+    async{
+        let request = WebRequest.Create(url)
+        let! response = request.AsyncGetResponse()
+        let stream = response.GetResponseStream()
+        let reader = new StreamReader(stream)
+        let! html = reader.AsyncReadToEnd()
+        return html}
+
+let downloadAllLinks url = 
+    async {
+        let! page = asyncFetch url
+        let urlRegExpr = new Regex(@"href=""http://?(\w|((?!\s|'|"")\W))*""")
+        let matches = urlRegExpr.Matches(page)
+        let tasks = [| for ref in matches ->  ref.Value.Substring(6, ref.Value.Length - 7) |> asyncFetch |]
+        let refs = [|for ref in matches -> ref.Value.Substring(6)|]
+        let! results = Async.Parallel tasks 
+
+        for i = 0 to results.Length - 1
+            do printfn "%s _contains_ %d" refs.[i] results.[i].Length}
+
+let showInfo  = Async.RunSynchronously << downloadAllLinks 
